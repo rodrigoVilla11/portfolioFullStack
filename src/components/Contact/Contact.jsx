53ddc2d22c5d845280cc52ed32c4b50441ba1e37
villaRodrigo11 <villarrealrodrigo.n@gmail.com>
2023-11-30 12:01:54 -0300

    All finished

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,7 @@
 import React,{useState} from 'react'
 import { useEffect } from 'react';
 import {useTranslation} from "react-i18next"
+import axios from "axios"
 
 
 const Contact = () => {
@@ -20,12 +21,63 @@ const Contact = () => {
     setForm({...form, [property]: value})
   }
 
-  const handleSubmit = (e) => {
+  // const sendEmail = () => {
+  //   window.Email.send({
+  //     SecureToken: '58e24c2b-1c71-4b5e-937d-3c1b7adfd95d', 
+  //     To: "villarrealrodrigo.n@gmail.com",
+  //     From: 'portfoliorodrigo3@gmail.com',
+  //     Subject: 'PORTFALIO CONTACT FROM ' + form.email,
+  //     Body: `Name: ${form.name}, Email: ${form.email} Message: ${form.message}`
+  //   }).then(
+  //     message => alert('Correo electrónico enviado con éxito: ' + message)
+  //   );
+  // };
+
+  //  const sendEmail = async () => {
+  //   const apiKey = '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';
+
+  //   try {
+  //     await axios.post('https://api.sendgrid.com/v3/mail/send', {
+  //       personalizations: [
+  //         {
+  //           to: [{ email: "villarrealrodrigo.n@gmail.com" }]
+  //         }
+  //       ],
+  //       from: { email: 'portfoliorodrigo3@gmail.com' },
+  //       subject:` PORTFALIO CONTACT FROM ${form.email}`,
+  //       content: [
+  //         {
+  //           type: 'text/plain',
+  //           value: `Name: ${form.name}, Email: ${form.email} Message: ${form.message}`
+  //         }
+  //       ]
+  //     }, {
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         Authorization: `Bearer ${apiKey}`
+  //       }
+  //     });
+
+  //     alert('Correo electrónico enviado con éxito');
+  //   } catch (error) {
+  //     console.error('Error al enviar el correo electrónico', error);
+  //     alert('Error al enviar el correo electrónico');
+  //   }
+  // };
+
+
+  const handleSubmit = async (e) => {
         if(!form.name || !form.email || !form.message){
             alert("Please complete the form correctly.");
         }else{
-            /*SEND EMAIL ACA */
-            alert("Message send successfully")
+          e.preventDefault()
+          try {
+            await axios.post('https://back-portfolio-jg2q.onrender.com/send-email', form);
+            alert('Email send successfully');
+          } catch (error) {
+            console.error('Error sending email', error);
+            alert('Error sending email');
+          }
             setForm({
                 name: '',
                 email: '',
@@ -33,6 +85,8 @@ const Contact = () => {
             })
         }
   }
+
+ 
   return (
     <div className=' bg-eggshell w-full h-screen flex flex-col justify-center items-center'>
         <h2 className='p-10 text-4xl '>{t("title")}</h2>
